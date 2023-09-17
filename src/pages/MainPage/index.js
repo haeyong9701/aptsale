0460fbbc7e9a0dbb1a6907ae950b1a36f37e3c32
haeyong9701 <haeyong9701@gmail.com>
2023-09-17 17:53:20 +0900

    chore 메인페이지 이미지 추가

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -11,11 +11,10 @@ const Mainpage = () => {
           <img src="images/main00.png" alt="메인페이지로고00" />
           <img src="images/main01.png" alt="메인페이지로고01" />
           <img src="images/main02.png" alt="메인페이지로고02" />
-          {/* <img src="images/main03.png" alt="메인페이지로고03" /> */}
-          {/* <img src="images/main04.png" alt="메인페이지로고04" /> */}
-          {/* <img src="images/main07.jpeg" alt="메인페이지로고07" /> */}
-          {/* <img src="images/main05.jpeg" alt="메인페이지로고05" /> */}
-          {/* <img src="images/main06.jpeg" alt="메인페이지로고06" /> */}
+          <img src="images/info01.png" alt="메인페이지로고03" />
+          <img src="images/type01.png" alt="메인페이지로고04" />
+          <img src="images/locate01.png" alt="메인페이지로고05" />
+          <img src="images/locate02.png" alt="메인페이지로고06" />
         </div>
       </Styled.Body>
       <Sidebar />
