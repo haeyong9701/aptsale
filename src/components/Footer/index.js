35f256b1a898e202fe5fbb1e2f865b7be8a80d01
haeyong9701 <haeyong9701@gmail.com>
2024-01-14 21:25:41 +0900

    feat: 사업자 정보 추가

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,6 +5,11 @@ const Footer = () => {
   return (
     <>
       <Styled.Footer>
+        <div className="info">
+          <div>사업명: 김해 삼계 푸르지오 센트럴파크</div>
+          <div>사이트 관리자: 조훈례</div>
+          <div>사업지 주소: 경상남도 김해시 삼계동 1027-3번지 일원</div>
+        </div>
         <div className="title">김해 삼계 푸르지오 센트럴파크</div>
         <div className="sub-title">
           대표번호 <span className="phone-number">1688 - 9427</span>
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Styled = {
   Footer: styled.div`
-    height: 200px;
+    height: 300px;
     background-color: #35363a;
     display: flex;
     justify-content: center;
@@ -12,6 +12,16 @@ export const Styled = {
     font-size: 1.5rem;
     @media screen and (max-width: 800px) {
       font-size: 3vw;
+    }
+
+    .info {
+      font-size: 0.9rem;
+      opacity: 0.8;
+      margin: 2px;
+    }
+
+    .info div {
+      margin: 4px;
     }
 
     .sub-title {
