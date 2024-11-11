22d910bb902e79c9370a78763cf3eca1e1854cfd
haeyong9701 <haeyong9701@gmail.com>
2024-11-11 21:59:36 +0900

    chore: footer 수정

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,14 +6,14 @@ const Footer = () => {
     <>
       <Styled.Footer>
         <div className="info">
-          <div>사업명: 김해 더샵 신문 그리니티</div>
-          <div>사업지 주소: 경상남도 김해시 신문1지구 A7-1블록</div>
+          <div>사업명: 쌍용 더 플래티넘 삼계</div>
+          <div>사업지 주소: 경상남도 김해시 삼계동 1027-12번지 일원</div>
           <div>사이트 관리자: 조훈례</div>
           <div>이메일: chr196667@naver.com</div>
           <div>전화번호 : 010-9316-8671</div>
           <div></div>
         </div>
-        <div className="title">김해 더샵 신문 그리니티</div>
+        <div className="title">쌍용 더 플래티넘 삼계</div>
         <div className="sub-title">
           대표번호 <span className="phone-number">1688 - 6331</span>
         </div>
