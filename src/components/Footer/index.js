2490e3f90cd62029ba225eded73f4a4537550def
haeyong9701 <haeyong9701@gmail.com>
2024-11-12 10:53:07 +0900

    chore: 메인 페이지 및 다른 페이지 수정

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,9 +8,9 @@ const Footer = () => {
         <div className="info">
           <div>사업명: 쌍용 더 플래티넘 삼계</div>
           <div>사업지 주소: 경상남도 김해시 삼계동 1027-12번지 일원</div>
-          <div>사이트 관리자: 조훈례</div>
+          {/* <div>사이트 관리자: 조훈례</div> */}
           <div>이메일: chr196667@naver.com</div>
-          <div>전화번호 : 010-9316-8671</div>
+          {/* <div>전화번호 : 010-9316-8671</div> */}
           <div></div>
         </div>
         <div className="sub-title">
