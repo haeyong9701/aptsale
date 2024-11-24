cf8a4119916bc46b02a8d50094977289922229e1
haeyong9701 <haeyong9701@gmail.com>
2024-11-24 18:31:42 +0900

    BottomBar 예약 링크 추가

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -6,16 +6,26 @@ import { ic_call } from "react-icons-kit/md/ic_call";
 const Sidebar = () => {
   return (
     <>
-      <Styled.Bar>
-        <div className="call">
-          <a href="tel:16886331" className="call-box">
-            <div className="icon">
-              <Icon size={"60"} icon={ic_call} />
+      <Styled.BottomBar>
+        <div className="call-container">
+          <a href="tel:16886331" className="call-link">
+            <div className="call-link-box">
+              <div>클릭시 상담사 연결</div>
+              <div className="call-icon-number">
+                <div className="icon">
+                  <Icon size={"20"} icon={ic_call} />
+                </div>
+                <div>1688-6331</div>
+              </div>
             </div>
-            <div>모델하우스 위치 안내</div>
           </a>
         </div>
-      </Styled.Bar>
+        <div className="reservation-container">
+          <a href="https://naver.me/FfeZ20DV" className="reservation-link">
+            <div>사전방문예약</div>
+          </a>
+        </div>
+      </Styled.BottomBar>
     </>
   );
 };
